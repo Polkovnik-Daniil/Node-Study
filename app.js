674c9d5e0f06b50777a9b6f88c58f56a803bff1f
Polkovnik-Daniil <0205danik@mail.ru>
2023-07-21 21:25:07 +0300

    Nodemon

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let name = process.argv[2];
-let age = process.argv[3];
-let val = process.argv[1];
-console.log("arg 1: " + val);
-console.log("name: " + name);
-console.log("age: " + age);
+const http = require("http");
+ 
+let message = "Hello World!";
+http.createServer(function(request,response){
+     
+    console.log(message);
+    response.end(message);
+     
+}).listen(3000, "127.0.0.1",()=>{
+    console.log("Сервер начал прослушивание запросов");
+});
