54665d43acd5b4f0d548440f4a2554726b1049af
Polkovnik-Daniil <0205danik@mail.ru>
2023-07-24 12:38:35 +0300

    Events class

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const Emitter = require("events");
-let emitter = new Emitter();
-let eventName = "greet"; //Объект без ивента
-
-//вешаем ивент на объект, но не вызываем
-emitter.on(eventName, function(){
-    console.log("Привет!");
-});
-//вешаем ивент на объект, но не вызываем
-emitter.on(eventName, function(){
-    console.log("Hello all!");
-});
-
+const EventEmitter = require("events");
+  
+let eventName = "greet";
  
-emitter.emit(eventName);
+class User extends EventEmitter {
+    sayHi(data) {
+        this.emit(eventName, data);
+    }
+}
+ 
+let user = new User();
+// добавляем к объекту user обработку события "greet"
+user.on(eventName, function(data){
+    console.log(data);
+});
+  
+user.sayHi("Мне нужна твоя одежда...");
