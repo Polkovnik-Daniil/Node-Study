bb17628eeb94df3412d994a305c778351ab9dd82
Polkovnik-Daniil <0205danik@mail.ru>
2023-07-27 17:33:10 +0300

    Pattern

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const http = require("http");
 const fs = require("fs");
  
-http.createServer(async (request, response) => {  
-    if (request.url === "/user") {
-         
-        const buffers = []; // буфер для получаемых данных
- 
-        for await (const chunk of request) {
-            buffers.push(chunk);        // добавляем в буфер все полученные данные
-        }
- 
-        const user = JSON.parse(Buffer.concat(buffers).toString());
-        console.log(user.name,"-", user.age);
-        response.end("Данные успешно получены");
-    }
-    else{
-        fs.readFile("index.html", (error, data) => response.end(data));
-    }
-}).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
+http.createServer(function(request, response){
+     
+    fs.readFile("index.html", "utf8", function(error, data){
+                 
+        let message = "Изучаем Node.js"; 
+        let header = "Главная страница";
+        data = data.replace("{header}", header).replace("{message}", message);
+        response.end(data);
+    })
+}).listen(3000);
