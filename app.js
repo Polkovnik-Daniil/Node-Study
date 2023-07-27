7b076002672af847021dfbd223b5fa072ddace9d
Polkovnik-Daniil <0205danik@mail.ru>
2023-07-27 18:43:36 +0300

    SendFile

--- app.js
+++ app.js
@@ -2,17 +2,8 @@ const express = require("express");
 const app = express();
  
 app.use(function (request, response) {
-  response.send(`<!DOCTYPE html>
-  <html>
-  <head>
-      <title>Главная</title>
-      <meta charset="utf-8" />
-  </head>
-  <body>
-      <h1>Главная страница</h1>
-      <h3>Привет, Express</h3>
-  </body>
-  <html>`);
+  response.sendFile(__dirname + "/index.html");
+  console.log("SUCCESS_SENT_FILE");
 });
  
 app.listen(3000);
