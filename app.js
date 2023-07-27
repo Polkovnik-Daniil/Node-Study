87b1e2231c00f4efc03e3dc3a8a6e2bd1c68be83
Polkovnik-Daniil <0205danik@mail.ru>
2023-07-27 18:41:26 +0300

    Отправка ответа, send

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 const express = require("express");
-const fs = require("fs");
- 
 const app = express();
-app.use(function(request, response, next){
-     
-    let now = new Date();
-    let hour = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
-    let data = `${hour}:${minutes}:${seconds} | ${request.method} | ${request.url} | ${request.get("user-agent")}`;
-    console.log(data);
-    fs.appendFile("server.log", data + "\n", function(){});
-    next();
+ 
+app.use(function (request, response) {
+    response.send("<h2>Hello</h2>");
 });
  
-app.get("/", function(request, response){
-    response.send("Hello");
-});
 app.listen(3000);
