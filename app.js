1f1850881c1cb4888ff4cf745fbe83066b65f835
Polkovnik-Daniil <0205danik@mail.ru>
2023-07-27 18:42:05 +0300

    Send(HTML)

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
  
 app.use(function (request, response) {
-    response.send("<h2>Hello</h2>");
+  response.send(`<!DOCTYPE html>
+  <html>
+  <head>
+      <title>Главная</title>
+      <meta charset="utf-8" />
+  </head>
+  <body>
+      <h1>Главная страница</h1>
+      <h3>Привет, Express</h3>
+  </body>
+  <html>`);
 });
  
 app.listen(3000);
