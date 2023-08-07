e42dc893c30c6c2b5f1016375b68ff8aa3217335
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-07 12:38:59 +0300

    Static file

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const app = express();
  
-app.use("/home/foo/bar",function (request, response) {
-  response.status(404).send(`Ресурс не найден`);
+const app = express();
+console.log(__dirname);
+app.use(express.static(__dirname + "/public"));
+app.use("/static", express.static(__dirname + "/public"));
+app.use("/", function(request, response){
+     
+    response.send("<h1>Главная страница</h1>");
 });
  
 app.listen(3000);
