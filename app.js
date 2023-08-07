698f2db03d6a2c6cb981b74cba743c7713320c42
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-07 14:05:54 +0300

    Send data to the application

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
 const express = require("express");
+
 const app = express();
-//302 можно использовать вместо 301
-app.use("/",function(request, response){
-    response.redirect(301, "/about");
+app.get("/", function (request, response) {
+  response.send("<h1>Главная страница</h1>");
 });
-app.use("/index", function (request, response) {
-  response.redirect("https://www.youtube.com/");
-});
-app.use("/home/bar", function (request, response) {
-  response.redirect("about");
-});
-app.use("/home/about", function (request, response) {
-  response.send("<h1>About OTHER</h1>");
-});
+//http://localhost:3000/home?id=3&name=poip
 app.use("/home", function (request, response) {
-  response.redirect("about");
+  let id = request.query.id;
+  let userName = request.query.name;
+  response.send(
+    "<h1>Информация</h1><p>id=" + id + "</p><p>name=" + userName + "</p>"
+  );
 });
+//http://localhost:3000/about?name=3&name=pop&name=pip
 app.use("/about", function (request, response) {
-  response.send("<h1>About MAIN</h1>");
+  console.log(request.query);
+  let names = request.query.name;
+  let responseText = "<ul>";
+  for (let i = 0; i < names.length; i++) {
+    responseText += "<li>" + names[i] + "</li>";
+  }
+  responseText += "</ul>";
+  response.send(responseText);
 });
-//Переадресация относительно текущего адреса на адрес на том же уровне:
-app.use("/home/f/bar", function (request, response) {
-  response.redirect("./about");
-});
-//Здесь идет переадресация с http://localhost:3000/home/f/bar на http://localhost:3000/home/f/about
-app.use("/home/fo/bar", function (request, response) {
-  response.redirect("../about");
+//http://localhost:3000/bar?user[id]=3&user[name]=pop
+app.use("/bar", function (request, response) {
+  console.log(request.query);
+  let id = request.query.user.id;
+  let name = request.query.user.name;
+  response.send("<h3>id:" + id + "<br>name: " + name + "</h3>");
 });
 
-//Здесь идет переадресация с http://localhost:3000/home/foo/bar на http://localhost:3000/home/foo
-app.use("/home/foo/bar", function (request, response) {
-  response.redirect(".");
-});
-//Здесь идет переадресация с http://localhost:3000/home/foo/bar на http://localhost:3000/home
-app.use("/home/fooo/bar", function (request, response) {
-  response.redirect("..");
-});
 app.listen(3000);
