c59a4f75a0b8153a36829c9f3f3f4dd69f03e071
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-08 18:46:46 +0300

    Router

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const app = express();
- 
-app.get("/products/:productId", function (request, response) {
-  response.send("productId: " + request.params["productId"])
+
+// определяем Router
+const productRouter = express.Router();
+
+// определяем маршруты и их обработчики внутри роутера
+productRouter.use("/create", function (request, response) {
+  response.send("Добавление товара");
+});
+productRouter.use("/:id", function (request, response) {
+  response.send(`Товар ${request.params.id}`);
+});
+productRouter.use("/", function (request, response) {
+  response.send("Список товаров");
+});
+// сопотавляем роутер с конечной точкой "/products"
+app.use("/products", productRouter);
+
+app.use("/about", function (request, response) {
+  response.send("О сайте");
 });
 
-app.get("/categories/:categoryId/products/:productId", function (request, response) {
-    let catId = request.params["categoryId"];
-    let prodId = request.params["productId"];
-    response.send(`Категория: ${catId}  Товар: ${prodId}`);
-});
-
-app.get("/book/:pageName.:pageExt", function (request, response) {
-    let pageName = request.params["pageName"];
-    let pageExt = request.params["pageExt"];
-    response.send(`Запрошенный файл: ${pageName}.${pageExt}`);
+app.use("/", function (request, response) {
+  response.send("Главная страница");
 });
 
 app.listen(3000);
