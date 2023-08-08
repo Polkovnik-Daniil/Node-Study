a809d71eefda91f11263cda67b795e5aaf6a8bf4
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-08 19:26:11 +0300

    Handlebars view engine 0.2

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 
 const app = express();
-//Чтобы установить Handlebars в качестве движка представлений в Express, вызывается функция ниже:
 app.set("view engine", "hbs");
-
+// установка пути к представлениям(вместо папки views будет использоваться папка templates)
+app.set("views", "templates"); 
 app.use("/contact", function (_, response) {
-  response.render("contact.hbs");
+  response.render("contact.hbs", {
+    title: "Мои контакты",
+    emailsVisible: true,
+    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+    phone: "+1234567890",
+  });
 });
 
 app.use("/", function (_, response) {
   response.send("Главная страница");
 });
-
 app.listen(3000);
