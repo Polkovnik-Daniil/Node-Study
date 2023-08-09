53dd017578d27f84c7f7676089a5a503cd59724d
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-09 14:43:25 +0300

    Engine EJS

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
 const express = require("express");
-const hbs = require("hbs");
 
 const app = express();
 
-hbs.registerHelper("getTime", function () {
-  const myDate = new Date();
-  const hour = myDate.getHours();
-  let minute = myDate.getMinutes();
-  let second = myDate.getSeconds();
-  if (minute < 10) {
-    minute = "0" + minute;
-  }
-  if (second < 10) {
-    second = "0" + second;
-  }
-  return `Текущее время: ${hour}:${minute}:${second}`;
-});
+app.set("view engine", "ejs");
 
-hbs.registerHelper("createStringList", function (array) {
-  let result = "";
-  for (let i = 0; i < array.length; i++) {
-    result += `<li>${array[i]}</li>`;
-  }
-  return new hbs.SafeString(`<ul>${result}</ul>`);
-});
-
-app.set("view engine", "hbs");
-
-app.get("/", function (_, response) {
-  response.render("home.hbs", {
-    fruit: ["apple", "lemon", "banana", "grape"],
+app.use("/contact", function (request, response) {
+  response.render("contact", {
+    title: "Мои контакты",
+    emailsVisible: true,
+    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+    phone: "+1234567890",
   });
+});
+app.use("/", function (request, response) {
+  response.send("Главная страница");
 });
 app.listen(3000);
