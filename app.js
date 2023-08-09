6aa09f5865225015e1aec763dae9fadd74f185dd
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-09 13:58:26 +0300

    Partial Views in Handlebars

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
-
+const hbs = require("hbs");
 const app = express();
+
 app.set("view engine", "hbs");
-// установка пути к представлениям(вместо папки views будет использоваться папка templates)
-app.set("views", "templates"); 
-app.use("/contact", function (_, response) {
-  response.render("contact.hbs", {
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use("/contact", function (request, response) {
+  response.render("contact", {
     title: "Мои контакты",
-    emailsVisible: true,
-    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+    email: "gavgav@mycorp.com",
     phone: "+1234567890",
   });
 });
 
-app.use("/", function (_, response) {
-  response.send("Главная страница");
+app.use("/", function (request, response) {
+  response.render("home.hbs");
 });
+
 app.listen(3000);
