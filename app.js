3bbab02f22a14eba77ac3aa18fc3d493f9f4c060
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-14 12:38:35 +0300

    MongoDB Collection

--- app.js
+++ app.js
@@ -2,23 +2,18 @@ const MongoClient = require("mongodb").MongoClient;
 
 const url = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(url);
+
 async function run() {
   try {
-    // Подключаемся к серверу
     await mongoClient.connect();
-    // обращаемся к базе данных admin
-    const db = mongoClient.db("admin");
-    // выполняем пинг для проверки подключения
-    const result = await db.command({ ping: 1 });
-    console.log("Подключение с сервером успешно установлено");
-    console.log(result);
+    const db = mongoClient.db("usersdb");
+    const collection = db.collection("users");
+    const count = await collection.countDocuments();
+    console.log(`В коллекции users ${count} документа/ов`);
   } catch (err) {
-    console.log("Возникла ошибка");
     console.log(err);
   } finally {
-    // Закрываем подключение при завершении работы или при ошибке
     await mongoClient.close();
-    console.log("Подключение закрыто");
   }
 }
 run().catch(console.error);
