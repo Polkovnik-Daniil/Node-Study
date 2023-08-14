15d794994fa90148be5965bd6d3c4a9250398a8b
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-14 12:31:48 +0300

    MongoDB

--- app.js
+++ app.js
@@ -1,118 +1,24 @@
-const express = require("express");
-const fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
 
-const app = express();
-const jsonParser = express.json();
-
-app.use(express.static(__dirname + "/public"));
-
-const filePath = "users.json";
-
-app.get("/api/users", function (req, res) {
-  const content = fs.readFileSync(filePath, "utf8");
-  const users = JSON.parse(content);
-  res.send(users);
-});
-// получение одного пользователя по id
-app.get("/api/users/:id", function (req, res) {
-  const id = req.params.id; // получаем id
-  const content = fs.readFileSync(filePath, "utf8");
-  const users = JSON.parse(content);
-  let user = null;
-  // находим в массиве пользователя по id
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].id == id) {
-      user = users[i];
-      break;
-    }
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
+async function run() {
+  try {
+    // Подключаемся к серверу
+    await mongoClient.connect();
+    // обращаемся к базе данных admin
+    const db = mongoClient.db("admin");
+    // выполняем пинг для проверки подключения
+    const result = await db.command({ ping: 1 });
+    console.log("Подключение с сервером успешно установлено");
+    console.log(result);
+  } catch (err) {
+    console.log("Возникла ошибка");
+    console.log(err);
+  } finally {
+    // Закрываем подключение при завершении работы или при ошибке
+    await mongoClient.close();
+    console.log("Подключение закрыто");
   }
-  // отправляем пользователя
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send();
-  }
-});
-// получение отправленных данных
-app.post("/api/users", jsonParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-
-  const userName = req.body.name;
-  const userAge = req.body.age;
-  let user = { name: userName, age: userAge };
-
-  let data = fs.readFileSync(filePath, "utf8");
-  let users = JSON.parse(data);
-
-  // находим максимальный id
-  const id = Math.max.apply(
-    Math,
-    users.map(function (o) {
-      return o.id;
-    })
-  );
-  // увеличиваем его на единицу
-  user.id = id + 1;
-  // добавляем пользователя в массив
-  users.push(user);
-  data = JSON.stringify(users);
-  // перезаписываем файл с новыми данными
-  fs.writeFileSync("users.json", data);
-  res.send(user);
-});
-// удаление пользователя по id
-app.delete("/api/users/:id", function (req, res) {
-  const id = req.params.id;
-  let data = fs.readFileSync(filePath, "utf8");
-  let users = JSON.parse(data);
-  let index = -1;
-  // находим индекс пользователя в массиве
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].id == id) {
-      index = i;
-      break;
-    }
-  }
-  if (index > -1) {
-    // удаляем пользователя из массива по индексу
-    const user = users.splice(index, 1)[0];
-    data = JSON.stringify(users);
-    fs.writeFileSync("users.json", data);
-    // отправляем удаленного пользователя
-    res.send(user);
-  } else {
-    res.status(404).send();
-  }
-});
-// изменение пользователя
-app.put("/api/users", jsonParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-
-  const userId = req.body.id;
-  const userName = req.body.name;
-  const userAge = req.body.age;
-
-  let data = fs.readFileSync(filePath, "utf8");
-  const users = JSON.parse(data);
-  let user;
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].id == userId) {
-      user = users[i];
-      break;
-    }
-  }
-  // изменяем данные у пользователя
-  if (user) {
-    user.age = userAge;
-    user.name = userName;
-    data = JSON.stringify(users);
-    fs.writeFileSync("users.json", data);
-    res.send(user);
-  } else {
-    res.status(404).send(user);
-  }
-});
-
-app.listen(3000, function () {
-  console.log("Сервер ожидает подключения...");
-});
+}
+run().catch(console.error);
