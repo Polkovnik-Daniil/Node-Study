a84457d53bd0e7401122d98fe1148a08549230fc
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-14 13:15:22 +0300

    findOneAndUpdate MongoDB

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ const url = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(url);
 
 async function run() {
+  const users = [
+    { name: "Bob", age: 34 },
+    { name: "Alice", age: 21 },
+    { name: "Tom", age: 45 },
+  ];
   try {
     await mongoClient.connect();
     const db = mongoClient.db("usersdb");
     const collection = db.collection("users");
-    //Удаляет Таблицу(Коллекцию)
-    const result = await collection.drop();
-    console.log(result); // true|false
+    await collection.insertMany(users);
+    const result = await collection.findOneAndUpdate(
+      { age: 21 },
+      { $set: { age: 25 } }
+    );
+    console.log(result);
   } catch (err) {
     console.log(err);
   } finally {
