cc2ddac9e9b049ba159dcda3b722487792f83ebf
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-14 12:51:40 +0300

    MongoDB Find

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(url);
 
-const users = [
-  { name: "Bob", age: 35 },
-  { name: "Alice", age: 21 },
-  { name: "Tom", age: 45 },
-];
-
 async function run() {
   try {
     await mongoClient.connect();
     const db = mongoClient.db("usersdb");
     const collection = db.collection("users");
-    const results = await collection.insertMany(users);
+    const results = await collection.find().toArray();
     console.log(results);
-    console.log(users);
   } catch (err) {
     console.log(err);
   } finally {
