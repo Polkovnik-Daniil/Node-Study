bbfc3486c56b68a020118ec8e18c83eb96ce363c
Polkovnik-Daniil <0205danik@mail.ru>
2023-08-14 12:46:52 +0300

    Adding multiple objects

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(url);
 
+const users = [
+  { name: "Bob", age: 35 },
+  { name: "Alice", age: 21 },
+  { name: "Tom", age: 45 },
+];
+
 async function run() {
   try {
     await mongoClient.connect();
     const db = mongoClient.db("usersdb");
     const collection = db.collection("users");
-    const user = { name: "Tom", age: 28 };
-    const result = await collection.insertOne(user);
-    console.log(result);
-    console.log(user);
+    const results = await collection.insertMany(users);
+    console.log(results);
+    console.log(users);
   } catch (err) {
     console.log(err);
   } finally {
